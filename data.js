e3cb81996b205872fdd7f97e2e6cbda45c40eee6
qroog <artemsvinin04@gmail.com>
2025-10-04 13:17:25 +0300

    Рекомендованные изменения

---
+++ data.js
@@ -0,0 +1,8 @@
+const products = [
+  { id: 1, name: "Moza R3", price: 24990, description: "FFB Руль", image: "Images/Moza_R3.WebP" },
+  { id: 2, name: "Moza R5", price: 39990, description: "FFB Руль", image: "Images/Moza_R5.WebP" },
+  { id: 3, name: "Moza R12", price: 45990, description: "FFB база", image: "Images/Moza_R12.WebP" },
+  { id: 4, name: "Simagic Sequential Shifter", price: 20990, description: "Секвентальный шифтер", image: "Images/Q1S.WebP" },
+  { id: 5, name: "Simagic GT1-D Wheel", price: 31990, description: "Рулевое колесо", image: "Images/Wheel.WebP" },
+  { id: 6, name: "Simagic P1000 Hydraulic Pedals", price: 57990, description: "Педали с обратной связью", image: "Images/Pedals.WebP" }
+];
